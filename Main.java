532d7e18d281f41d3f4042b51c13e52d57ed8043
Elmurodov Javohir <74240915+jlkesh@users.noreply.github.com>
2022-06-07 11:28:55 +0500

    just comment

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import java.util.Date;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Date());
+        System.out.println("sadasdsad);
         System.out.println("Hello World");
     }
 }
