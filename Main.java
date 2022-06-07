78f68e7fb46e4ba6203622c4d158afc21d02a7ac
jlkesh <john.lgd65@gmail.com>
2022-06-07 11:22:57 +0500

    initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author "Elmurodov Javohir"
+ * @since 07/06/22 11:19 (Tuesday)
+ * git/IntelliJ IDEA
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
